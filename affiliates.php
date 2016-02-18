a994b27a8eaa404f7d105e796fedc5d445b5eb99
Mitchell Ludwig <mitchell.ludwig@gmail.com>
2016-02-18 00:51:40 -0700

    Updated affiliates

--- affiliates.php
+++ affiliates.php
@@ -11,10 +11,118 @@
 	<?php include("fragments/nav.php"); ?>
 
 
-		<div id="content" class="container-fluid">
+		<div id="content" class="container-fluid text-center">
 			<p>These are some people who I think are awesome, check them out!</p>
-			<p><a href="http://edgemontgeek.com/">Mitchell Ludwig</a></p>
-			<p><a href="http://emilystener.com/">Emily Stener</a></p>
+			
+            
+            <div class="tile">
+                <a href="http://www.http://erasmus-joe.tumblr.com//">Amanda N.</a>
+                <a href="http://www.http://erasmus-joe.tumblr.com//" class="affiliatespic amandan"></a>
+            </div>
+            
+            <div class="tile">
+                <a href="http://www.artstation.com/artist/amycornelson/">Amy C.</a>
+                <a href="http://www.artstation.com/artist/amycornelson/" class="affiliatespic amyc"></a>
+            </div>
+            
+            <div class="tile">
+                <a href="https://soundcloud.com/averymitchell/">Avery P.M.</a>
+                <a href="https://soundcloud.com/averymitchell/" class="affiliatespic avery"></a>
+            </div>
+            
+            <div class="tile">
+                <a href="http://www.attainablerecords.com/">Connor H.D.</a>
+                <a href="http://www.attainablerecords.com/" class="affiliatespic attainable"></a>
+            </div>
+            
+            <div class="clearfix"></div>
+            
+            <div class="tile">
+                <a href="http://www.soundcloud.com/laozimusic/">Cory D.P.</a>
+                <a href="http://www.soundcloud.com/laozimusic/" class="affiliatespic corydp"></a>
+            </div>
+            
+            <div class="tile">
+                <a href="http://www.facebook.com/newgameplusca/">Daniel B.</a>
+                <a href="http://www.facebook.com/newgameplusca/" class="affiliatespic newgameplus"></a>
+            </div>
+            
+            <div class="tile">
+                <a href="http://www.dare-photo.com/">Derek N.</a>
+                <a href="http://www.dare-photo.com/" class="affiliatespic derek"></a>
+            </div>
+            
+            <div class="tile">
+                <a href="http://emilystener.com/">Emily S.</a>
+                <a href="http://emilystener.com/" class="affiliatespic emily"></a>
+            </div>
+            
+            <div class="clearfix"></div>
+            
+            <div class="tile">
+                <a href="http://www.blindroots.com/">Gus R.</a>
+                <a href="http://www.blindroots.com/" class="affiliatespic gus"></a>
+            </div>
+            
+            <div class="tile">
+                <a href="http://www.jadofalltrades.guru/">Jad M.</a>
+                <a href="http://www.jadofalltrades.guru/" class="affiliatespic jad"></a>
+            </div>
+            
+            <div class="tile">
+                <a href="http://www.soundcloud.com/pixelsofmind/">Jay G.</a>
+                <a href="http://www.soundcloud.com/pixelsofmind/" class="affiliatespic pixelsofmind"></a>
+            </div>
+            
+            <div class="tile">
+                <a href="http://www.kaitlynhoover.com/">Kaitlyn H.</a>
+                <a href="http://www.kaitlynhoover.com/" class="affiliatespic hoover"></a>
+            </div>
+            
+            <div class="clearfix"></div>           
+            
+            <div class="tile">
+                <a href="http://edgemontgeek.com/">Mitchell L.</a>
+                <a href="http://edgemontgeek.com/" class="affiliatespic mitchell"></a>
+            </div>
+        
+            <div class="tile">
+                <a href="http://www.myannguyen.com/">My-An N.</a>
+                <a href="http://www.myannguyen.com/" class="affiliatespic myan"></a>
+            </div>
+        
+            <div class="tile">
+                <a href="http://www.nickj.ca/">Nick J.</a>
+                <a href="http://www.nickj.ca/" class="affiliatespic nickj"></a>
+            </div>
+
+            <div class="tile">
+                <a href="http://www.luckyhitcrew.com/">Nigz B.M.</a>
+                <a href="http://www.luckyhitcrew.com/" class="affiliatespic nigzblackman"></a>
+            </div>
+
+             <div class="clearfix"></div>
+            
+            <div class="tile">
+                <a href="http://www.quickdrawanimation.ca/">QuickDraw</a>
+                <a href="http://www.quickdrawanimation.ca/" class="affiliatespic quickdraw"></a>
+            </div>
+            
+            <div class="tile">
+                <a href="http://www.brokenlightart.com/">Thomas P.</a>
+                <a href="http://www.brokenlightart.com/" class="affiliatespic thomas"></a>
+            </div>
+
+            <div class="tile">
+                <a href="http://www.tklongmire.com/">Tyler K.L.</a>
+                <a href="http://www.tklongmire.com/" class="affiliatespic tkl"></a>
+            </div>
+                     
+            <div class="tile">
+                <a href="http://www.victoruzunow.com/">Victor U.</a>
+                <a href="http://www.victoruzunow.com/" class="affiliatespic uzunow"></a>
+            </div>                                       
+                            
 		</div>
 
 	<?php include("fragments/footer-all.php"); ?>
